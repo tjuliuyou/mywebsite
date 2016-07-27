64e632aebb68bcfb55fca6ab0122b8e1a655d6cd
刘优 <tjuliuyou@gmail.com>
2016-07-27 10:33:32 +0800

    Ignore sensitive files

--- config/deploy.rb
+++ config/deploy.rb
@@ -27,7 +27,7 @@ set :scm, :git
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, fetch(:linked_files, []).push('config/database.yml', 'config/application.yml','config/secrets.yml')
+set :linked_files, fetch(:linked_files, []).push('config/database.yml','config/secrets.yml')
 
 # Default value for linked_dirs is []
 set :linked_dirs, fetch(:linked_dirs, []).push('bin','log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'public/system')
